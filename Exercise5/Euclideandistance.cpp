bda2dedd08ae276013b41ace314ba92eadd90a49
NguyenManh <nguyenmanh200391@gmail.com>
2023-10-13 08:42:32 +0700

    Euclidean distance
    tính khoảng cách euclid giữa 2 điểm có tọa độ cho sẵn!

---
+++ Exercise5/Euclideandistance.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+#include<math.h>
+
+using namespace std;
+
+int main() {
+	int x1, y1, x2, y2;
+	cin >> x1 >> y1 >> x2 >> y2;
+	double Ec = sqrt(pow(x1 - x2, 2) + (pow(y1 - y2, 2)));
+	cout << fixed << setprecision(2) << Ec << endl;
+	return 0;
+
+}
