88eb1eb675fb10da6998417fc1936fa4674885d2
NguyenManh <nguyenmanh200391@gmail.com>
2023-10-13 16:04:58 +0700

    check characters
    kiểm tra kí tự in thường

---
+++ Exercise14/checkcharacters.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main() {
+	char n;
+	cin>> n;
+	if (n >= 'a' && n <= 'z') { cout << "YES" << endl;}
+	else { cout << "NO" << endl; }
+	return 0;
+}
