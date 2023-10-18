35b4e596b446a16e190e0e73c8ecc04fba1294fe
NguyenManh <nguyenmanh200391@gmail.com>
2023-10-18 12:25:45 +0700

    prọect1

---
+++ Project1/FileName.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<iomanip>
+#include<math.h>
+
+using namespace std;
+int T(int a) {
+	int s = a * a;
+	cout << s << endl;
+	return s;
+
+}
+
+int main() {
+	int e, f;
+	cin >> e >> f;
+	T(2); {
+		int h = e + s + f;
+	}
+	return 0;
+
+
+
+}
