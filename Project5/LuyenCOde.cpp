365b0ebb3a992774ecec3189bb20fdd3eee5ce52
NguyenManh <nguyenmanh200391@gmail.com>
2023-10-25 20:19:36 +0700

    cd

---
+++ Project5/LuyenCOde.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+#include<iomanip>
+#include<math.h>
+
+using namespace std;
+
+//int main() {
+//	char TEn[30];
+//	cout << "xin moi nhap ten" << endl;
+//	cin.getline(TEn, 30);
+//	cout << "Ten co ay la\n" << TEn << endl;
+//	return 0;
+//}
+
+//#include<cstring>
+//
+//int main() {
+//	char Skteam[] = "Kteam";
+//	cout <<"Do dai:"
+
+//int main() {
+//	int a, b, c, d;
+//	cin >> a >> b >> c >> d;
+//
+//
+//	if (a > b && c > d) {
+//		cout << a + b + c + d << endl;
+//                    }
+//	else if (b > a && d > c) { cout << a - b - c - d << endl; }
+//	else if (a > c && b > d) {
+//		cout << a * b + a * c << endl;
+//	}
+//
+//	return 0;
+//}
+#include<vector>
+// xem kic thước mảng kiểu std::vector để xem kíc thước của mảng ta dùng hàm size():
+//  VD 
+int main() {
+	vector <int> arr = { 2,5,3,2,1 };
+	// xem kic thước mảng kiểu std::vector để xem kíc thước của mảng ta dùng hàm size():
+	cout << arr.size() << endl;
+}
+		
