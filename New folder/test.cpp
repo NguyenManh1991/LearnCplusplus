7bd58734486702102e220334c856aa221f86666b
NguyenManh <nguyenmanh200391@gmail.com>
2023-11-14 10:47:41 +0700

    opengl

---
+++ New folder/test.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    if (argc >1)
+    {
+            cout << argv[1] << endl;
+    }
+if(argc >2)
+{
+    cout << argv[2];
+}
+
+    
+    return 0;
+}
