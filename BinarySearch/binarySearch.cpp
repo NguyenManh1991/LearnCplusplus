c1658bebafe63da226964a8fd3ff4ce5cab3a0b9
NguyenManh <nguyenmanh200391@gmail.com>
2023-12-10 10:51:55 +0700

    binarySearch

---
+++ BinarySearch/binarySearch.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<algorithm>
+
+
+//THuật Toán Tìm kiếm tuyến tính linear search
+// Thuật toán sẽ duyệt tất cả các phần tử trong mảng để tìm kiếm đối tượng
+
+bool ls(int a[], int n, int x) {
+	for (int i = 0; i < n; i++) {
+		if (a[i] = x) {
+			return true;
+		}
+		else return false;
+	}
+	
+}
+
+int main() {
+	int a[5] = { 1,2,3,4,5 };
+	ls(a, 5, 2); std::cout << *ls << std::endl;
+	return 0;
+}
+
+// Binary search : Thuật toán tìm kiếm nhị phân
+// Điều kiện là các phần tử trong mảng hoặc vector đều đã được sắp xếp!
